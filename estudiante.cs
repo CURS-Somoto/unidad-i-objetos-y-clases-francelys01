b4758c9922f959477a8de2df6a6bf7828866f39e
Francelys Raquel Vanega Rodríguez <147260983+francelys01@users.noreply.github.com>
2023-10-21 08:40:40 -0600

    Update estudiante.cs
    entrega de tarea

--- estudiante.cs
+++ estudiante.cs
@@ -2,12 +2,28 @@ using System;
 
 public class Estudiante
 {
-   
-    public string Nombre { get; set; }
-    public int Edad { get; set; }
-    public string Curso { get; set; }
+  
+           Nombre = "Ernesto José Diaz Herrera";
+            Edad = 18;
+            Curso = "Primer año de Ingenieria en Sistema";
+        
+        }
 
-    
+        public Estudiante(string NombreEstudiante, int EdadEstudiante, string CursoEstudiante)
+        {
+            Nombre = NombreEstudiante;
+            Edad = EdadEstudiante;
+            Curso = CursoEstudiante;
+        }
+
+        public string getInfoEstudiante()
+        {
+            return "Información del estudiante:\n " +  " Nombre: " + Nombre + "      Edad:" + " " + Edad + "      Curso: " +  Curso;
+        }
+        private string Nombre;
+        private int Edad;
+        private string Curso;
+   } 
    
 }
 
@@ -15,6 +31,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("¡Hola, mundo!");
+           Estudiante estudiante1 = new Estudiante(); // crear objeto/instancia de tipo estudiante.
+            // dar un estado Inicial al estudiante.
+            
+            Console.WriteLine(estudiante1.getInfoEstudiante());
+
+            Estudiante estudiante2 = new Estudiante("Anielka Vanesa Arteaga Gómez", 20, "Tercer año de Adminstración de Empresa");
+
+            Console.WriteLine(estudiante2.getInfoEstudiante());
+            Console.ReadKey();
     }
 }
