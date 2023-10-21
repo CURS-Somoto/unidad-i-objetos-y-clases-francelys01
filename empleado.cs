7d5f35e02c49a6d45a34088b62d919813a6fb7d8
Francelys Raquel Vanega Rodríguez <147260983+francelys01@users.noreply.github.com>
2023-10-21 08:13:04 -0600

    Update empleado.cs
    Entrega de tarea

--- empleado.cs
+++ empleado.cs
@@ -9,6 +9,7 @@ public class Empleado
     
     public double CalcularSalarioAnual()
     {
+       return  Salario * 12;
         
     }
 }
@@ -17,6 +18,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("¡Hola, mundo!");
+            Empleado empleado1 = new Empleado();
+            empleado1.Nombre = "Stefani Valeria Aguilar Estrada";
+            empleado1.Salario = 3000;
+
+            Empleado empleado2 = new Empleado();
+            empleado2.Nombre = "Jonathan Alexander Espinoza Herrera";
+            empleado2.Salario = 6000;
+
+            Console.WriteLine("El salario anual de " +empleado1.Nombre+ " es de " + empleado1.CalcularSalarioAnual());
+            Console.WriteLine("El salario anual de " +empleado2.Nombre+ " es de " + empleado2.CalcularSalarioAnual());
+
+            Console.ReadKey();
+
+        }
     }
 }
