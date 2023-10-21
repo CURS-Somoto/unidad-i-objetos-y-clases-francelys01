bb24b5a5e01d10a7d79c9f6cacf68720d60b355a
Francelys Raquel Vanega Rodríguez <147260983+francelys01@users.noreply.github.com>
2023-10-21 09:32:48 -0600

    Update libro.cs
    Entrega de tarea

--- libro.cs
+++ libro.cs
@@ -3,9 +3,33 @@ using System;
 public class Libro
 {
     // Propiedades
-    public string Título { get; set; }
-    public string Autor { get; set; }
+ 
+ private String Autor;
 
+        public String Autor1
+        {
+            get { return Autor; }
+            set { Autor = value; }
+        }
+        private String Título;
+
+        public String Título1
+        {
+            get { return Título; }
+            set { Título = value; }
+        }
+
+        public Libro(String tit, String aut)
+        {
+            Título = tit;
+            Autor = aut;
+        }
+        public override string ToString()
+        {
+            return "Título : " + Título + "\n" + "Autor: " + Autor;
+        }
+    }
+}
   
    
 }
@@ -14,6 +38,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("¡Hola, mundo!");
+            Libro lib1 = new Libro("El programador progmático", "Andy Hunt");
+
+            Libro lib2 = new Libro("Clean Code", "Robert C Martin");
+
+            Console.WriteLine(lib1);
+            Console.WriteLine(lib2);
+            Console.ReadKey();
     }
 }
